d0181144737a76715db2e279dbc9918c68ced290
Kat <katjabensiek@gmail.com>
2019-05-22 14:11:37 +0200

    lorenz curve for romania and south africa

--- income_dis/incomedashboard/app.py
+++ income_dis/incomedashboard/app.py
@@ -17,21 +17,36 @@ trace1 = dict(x=v['year'], y=v['gini'], text=v['index'], type='scatter', mode='m
 v = v.reset_index().set_index('year')
 trace2 = dict(x=v.index, y=v['gini'], text=v['continents'], type='box',
               transforms=[dict(type='groupby', groups=v.index)])
+rom = df[df['index'] == 'Romania'][['year', '%inc_highest20','%inc_fourth20','%inc_third20','%inc_second20' ,'%inc_low20']].set_index('year')
+sa = df[df['index'] == 'South Africa'][['year', '%inc_highest20',
+                                    '%inc_fourth20','%inc_third20', '%inc_second20',
+                                    '%inc_low20']].set_index('year')
 
+trace3 = dict(y=[100] + list(rom.iloc[0]) + [0], x=[100, 80, 60, 50, 40, 20, 0], type='scatter')
+trace0 = dict(x=[0, 100], y=[0, 100], type='scatter')
+trace4 = dict(y=[100] + list(sa.iloc[0]) + [0], x=[100, 80, 60, 50, 40, 20, 0], type='scatter')
+
+data1 = [trace4, trace0]
+data2 = [trace3, trace0]
 
 data3 = [trace1, trace2]
 
 layout = dict(title='Gini coefficient per continent',
               hovermode='closest', xaxis=dict(ticks=str(range(1986, 2007))), showticklabels=True, dtick=1,
               tickvals=list(GDc['year']))
+layout1 = dict(title= 'Lorenz curve Romania 1989', hovermode='closest', showlegend=False, xaxis=go.layout.XAxis(
+        title=go.layout.xaxis.Title(text='% of population')), yaxis=go.layout.YAxis(title=go.layout.yaxis.Title(text='% of income')))
+layout2 = dict(title= 'Lorenz curve South Africa 1993', hovermode='closest', showlegend=False, xaxis=go.layout.XAxis(
+        title=go.layout.xaxis.Title(text='% of population')), yaxis=go.layout.YAxis(title=go.layout.yaxis.Title(text='% of income')))
 figt = dict(data=data3, layout=layout)
-
+figrom = dict(data=data2, layout=layout1)
+figsa = dict(data=data1, layout=layout2)
 
 app = dash.Dash(__name__)
 
 app.layout = html.Div([
     html.Div([
-        html.H2('I N C O M E -  I N E Q U A L I T Y '),
+        html.H2('M E A S U R I N G - I N E Q U A L I T Y'),
         html.Img(src='/assets/stats.png'),
     ], className='banner'),
 
@@ -47,9 +62,14 @@ app.layout = html.Div([
                      value='2000'),
     dcc.Graph(id='gini_by_year')], className='three columns'),
     html.Div([
+        dcc.Graph(id='romania', figure=figrom)], className='five columns'),
+    html.Div([
+        dcc.Graph(id='southafrica', figure=figsa)
+    ], className='five columns'),
+    html.Div([
         dcc.Graph(id='stats-world',
                   figure=figt)
-    ], className='twelve columns'),
+    ], className='twelve columns')
 
 ], className='row')
 
@@ -116,4 +136,5 @@ def bar_grap(year):
 
 if __name__ == '__main__':
     app.run_server(debug=True)
+
 
